e6a5b848379f40ac39bc08d2531318af16d8086a
MaxFray8908 <nadja.gawrilowa@yandex.ru>
2022-02-03 13:06:31 +0400

    feat: add main functional

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let start = [];
+  let end = [];
+  let similar = [];
+  let count = [];
+  
+  for (let item of bracketsConfig) {
+      if (item[0] === item[1]) {
+          similar.push(item[0]);
+      }
+      start.push(item[0]);
+      end.push(item[1]);
+  }
+  
+  for (let index = 0; index < str.length; index++) {
+      
+      if (start.includes(str[index])) {
+          if (similar.includes(str[index]) && count[count.length - 1] === start.indexOf(str[index])) {
+              count.pop();
+              continue;
+          }
+          count.push(start.indexOf(str[index]));
+      }
+      else {
+          let poisk = count.indexOf(end.indexOf(str[index]), count.length - 1);
+          if (poisk !== -1) {
+              count.pop();
+          }
+          else {
+              return false;
+          }
+      }
+  }
+  
+  return count.length === 0 ? true : false;
 }
